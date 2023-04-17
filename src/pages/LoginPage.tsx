eebdb3a8df761f5974043dcabfd3ef641d3106ee
Fauzan <fauzan.linnas@ifg-life.id>
2023-04-17 20:48:44 +0700

    add react hook form

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -15,12 +15,19 @@ import { useDispatch } from "react-redux";
 import { login } from "../store/actions/authActions";
 import { useNavigate } from "react-router-dom";
 import { EmailIcon, Logo, PasswordIcon } from "../assets";
+import { useForm } from "react-hook-form";
 
 const LoginPage = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const handleLogin = () => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = () => {
     // Dispatch the login action
     dispatch(login());
     navigate("/chat");
@@ -43,34 +50,60 @@ const LoginPage = () => {
           <Text textAlign="left">Masuk akun untuk menggunakan PituChat </Text>
         </Box>
         <Box display="flex" flexDirection="column">
-          <FormControl mb={6}>
-            <FormLabel htmlFor="email">Email</FormLabel>
-            <InputGroup>
-              <InputLeftElement pointerEvents="none" children={<EmailIcon />} />
-              <Input type="email" id="email" name="email" />
-            </InputGroup>
-          </FormControl>
-          <FormControl mb={2}>
-            <FormLabel htmlFor="password">Password</FormLabel>
-            <InputGroup>
-              <InputLeftElement
-                pointerEvents="none"
-                children={<PasswordIcon />}
-              />
-              <Input type="password" id="password" name="password" />
-            </InputGroup>
-          </FormControl>
-          <Link color={"#808080"} fontSize={"sm"} mb={6} alignSelf="flex-end">
-            Lupa password?
-          </Link>
-          <Button
-            w="full"
-            bgColor="#0C4AC0"
-            onClick={handleLogin}
-            color="white"
-          >
-            Login
-          </Button>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <FormControl mb={6} isInvalid={!!errors.email}>
+              <FormLabel htmlFor="email">Email</FormLabel>
+              <InputGroup>
+                <InputLeftElement
+                  pointerEvents="none"
+                  children={<EmailIcon />}
+                />
+                <Input
+                  type="email"
+                  id="email"
+                  {...register("email", {
+                    required: "This field is required",
+                    pattern: {
+                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                      message: "Invalid email address",
+                    },
+                  })}
+                />
+              </InputGroup>
+              {errors.email && (
+                <Text color="red.500" fontSize="sm" mt={1}>
+                  {errors.email.message as string}
+                </Text>
+              )}
+            </FormControl>
+            <FormControl mb={2} isInvalid={!!errors.password}>
+              <FormLabel htmlFor="password">Password</FormLabel>
+              <InputGroup>
+                <InputLeftElement
+                  pointerEvents="none"
+                  children={<PasswordIcon />}
+                />
+                <Input
+                  type="password"
+                  id="password"
+                  {...register("password", {
+                    required: "Password is required",
+                  })}
+                />
+              </InputGroup>
+              {errors.password && (
+                <Text color="red.500" fontSize="sm" mt={1}>
+                  {errors.password.message as string}
+                </Text>
+              )}
+            </FormControl>
+            <Link color="#808080" fontSize="sm" mb={6} alignSelf="flex-end">
+              Lupa password?
+            </Link>
+            <Button w="full" bgColor="#0C4AC0" color="white" type="submit">
+              Login
+            </Button>
+          </form>
         </Box>
       </Flex>
     </Flex>
